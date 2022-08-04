557afa85c59b3931cf94bdff6cc93cd4a52d7b1e
= <=>
2022-08-04 21:48:49 +0700

    crawls

---
+++ src/crawls/Anime/dataAnime.java
@@ -0,0 +1,39 @@
+package crawls.Anime;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class dataAnime {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://animehay.club/");
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+            // regex
+            Pattern p = Pattern.compile("name-movie\">(.*?)</div>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+// close scanner
+            scanner.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
