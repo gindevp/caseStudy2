52c717a54c0c86ed2b0b82c5594aa908ba458f0e
= <=>
2022-08-05 09:07:52 +0700

    update crawls

--- src/crawls/DataAnime.java
+++ src/crawls/DataAnime.java
@@ -1,18 +1,23 @@
-package crawls.Anime;
+package crawls;
+
+import storage.FileWriteRead;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class dataAnime {
+public class DataAnime {
     public static void main(String[] args) {
         try {
             URL url = new URL("https://animehay.club/");
+            List<String> list= new ArrayList<>();
             // open the stream and put it into BufferedReader
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
@@ -24,7 +29,9 @@ public class dataAnime {
             Matcher m = p.matcher(content);
             while (m.find()) {
                 System.out.println(m.group(1));
+                list.add(m.group(1));
             }
+            new FileWriteRead().writeFile(list,"src/crawls/anime.dat");
 // close scanner
             scanner.close();
 
